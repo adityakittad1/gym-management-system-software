f835f460896fbc6e51f406bf71482835ba19decc
Aditya Kittad <adityakittad1054@gmail.com>
2026-06-26 21:14:30 +0530

    fix(backend): remove custom puppeteer cache config to use global cache

--- server/.puppeteerrc.cjs
+++
@@ -1,9 +0,0 @@
-const {join} = require('path');
-
-/**
- * @type {import("puppeteer").Configuration}
- */
-module.exports = {
-  // Changes the cache location for Puppeteer to an un-ignored directory
-  cacheDirectory: join(__dirname, 'puppeteer-browsers'),
-};
